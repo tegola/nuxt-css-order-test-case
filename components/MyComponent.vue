631b64db63d0a81368453cbb21cfd122de83525a
Alan Sprecacenere <alan@qreate.it>
2020-05-01 00:07:45 +0200

    First commit

---
+++ components/MyComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="my-component" />
+</template>
+
+<style>
+.my-component {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="my-page">
+    <my-component class="my-page__component" />
+    <br>
+    <p>The box above is blue (correct) on a dev environment and red (wrong) after building the project.</p>
+  </div>
+</template>
+
+<script>
+import MyComponent from '~/components/MyComponent.vue'
+
+export default {
+  components: {
+    MyComponent
+  }
+}
+</script>
+
+<style>
+.my-page {
+  margin: 5rem;
+}
+.my-page__component {
+  background-color: blue;
+}
+</style>
