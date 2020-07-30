7b21d359218fe228ab9a306c69c923c37f695555
Alan Sprecacenere <alan@qreate.it>
2020-07-30 18:14:01 +0200

    Upgrade to nuxt 2.14

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
+    // 'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
   ],
